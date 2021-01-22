9495dbf1ef96d58cd5a56a3f91842aa31641ac80
Simon Fell <fellforce@gmail.com>
2021-01-22 13:32:35 -0800

    have most tests more comprehensively check the tree state

--- art_test.go
+++ art_test.go
@@ -3,6 +3,9 @@ package art
 import (
 	"bytes"
 	"fmt"
+	"reflect"
+	"sort"
+	"strings"
 	"testing"
 )
 
@@ -17,162 +20,134 @@ func Test_OverwriteWithSameKey(t *testing.T) {
 	k := []byte("QWE")
 	a.Insert(k, "one")
 	hasStringKey(t, a, "QWE", "one")
+	hasKeyVals(t, a, []keyVal{kvs("QWE", "one")})
 	a.Insert(k, "two")
 	hasStringKey(t, a, "QWE", "two")
-}
-
-func Test_InsertGet(t *testing.T) {
-	a := new(Art)
-	a.Insert([]byte("123"), "abc")
-	hasStringKey(t, a, "123", "abc")
-	noStringKey(t, a, "2")
-	noStringKey(t, a, "12")
-	noStringKey(t, a, "1234")
-	noStringKey(t, a, "")
+	hasKeyVals(t, a, []keyVal{kvs("QWE", "two")})
 }
 
 func Test_InsertOnLeaf(t *testing.T) {
-	a := new(Art)
-	a.Insert([]byte("123"), "abc")
-	// now insert something that would add a child to the leaf above
-	a.Insert([]byte("1234"), "abcd")
-	hasStringKey(t, a, "123", "abc")
-	hasStringKey(t, a, "1234", "abcd")
+	testArt(t, []keyVal{
+		kvs("123", "abc"),
+		// now insert something that would add a child to the leaf above
+		kvs("1234", "abcd"),
+	}, nil)
 }
 
 func Test_LeafPathToNToLeafPath(t *testing.T) {
-	a := new(Art)
-	k1 := []byte("123")
-	k2 := []byte("12345678")
-	a.Insert(k1, "1")
-	a.Insert(k2, "2")
-	hasStringKey(t, a, "123", "1")
-	hasStringKey(t, a, "12345678", "2")
+	testArt(t, []keyVal{
+		kvs("123", "1"),
+		kvs("12345678", "2"),
+	}, nil)
 }
 
 func Test_MultipleInserts(t *testing.T) {
-	a := new(Art)
-	a.Insert([]byte("123"), "abc")
-	a.Insert([]byte("456"), "abcd")
-	a.Insert([]byte("1211"), "abcde")
-	hasStringKey(t, a, "123", "abc")
-	hasStringKey(t, a, "456", "abcd")
-	hasStringKey(t, a, "1211", "abcde")
-	noStringKey(t, a, "")
-	noStringKey(t, a, "12")
-	noStringKey(t, a, "1234")
-	noStringKey(t, a, "5")
-	noStringKey(t, a, "451")
-	noStringKey(t, a, "4561")
-	noStringKey(t, a, "1212")
-	noStringKey(t, a, "121")
+	testArt(t, []keyVal{
+		kvs("123", "abc"),
+		kvs("456", "abcd"),
+		kvs("1211", "def"),
+	}, nil)
 }
 
 func Test_Grow4to16(t *testing.T) {
-	a := new(Art)
+	keyVals := []keyVal{}
 	k := []byte{65, 66}
 	for i := byte(0); i < 10; i++ {
-		a.Insert(append(k, i), i)
-		for j := byte(0); j <= i; j++ {
-			hasByteKey(t, a, append(k, j), j)
-		}
+		keyVals = append(keyVals, kv(append(k, i), i))
 	}
-	a.Insert(append(k, 5, 10), 100)
-	hasByteKey(t, a, append(k, 5, 10), 100)
+	keyVals = append(keyVals, kv(append(k, 5, 10), 100))
+	testArt(t, keyVals, nil)
 }
 
 func Test_GrowTo48(t *testing.T) {
-	a := new(Art)
-	k := []byte("12")
-	a.Insert(k, "12")
-	hasStringKey(t, a, "12", "12")
-	for i := byte(0); i < 30; i++ {
-		a.Insert(append(k, i+'A'), fmt.Sprintf("val_%d", i))
-		for j := byte(0); j <= i; j++ {
-			nk := append(k, j+'A')
-			hasByteKey(t, a, nk, fmt.Sprintf("val_%d", j))
-			hasStringKey(t, a, "12", "12")
-		}
+	keyVals := []keyVal{}
+	k := []byte{65, 66}
+	for i := byte(0); i < 40; i++ {
+		keyVals = append(keyVals, kv(append(k, i), i))
 	}
-	noStringKey(t, a, "12z")
-	noStringKey(t, a, "12Bzasd")
-	noStringKey(t, a, "12Bz")
+	keyVals = append(keyVals, kv(append(k, 5, 10), 100))
+	testArt(t, keyVals, nil)
 }
 
 func Test_GrowTo256(t *testing.T) {
-	a := new(Art)
+	keyVals := []keyVal{}
+	k := []byte{65, 66, 67}
 	for i := 0; i < 256; i++ {
-		a.Insert([]byte{byte(i)}, i)
+		keyVals = append(keyVals, kv(append(k, byte(i)), i))
 	}
-	for i := 0; i < 256; i++ {
-		hasByteKey(t, a, []byte{byte(i)}, i)
-	}
+	keyVals = append(keyVals, kv(append(k, 5, 10), 100))
+	testArt(t, keyVals, nil)
 }
 
 func Test_GrowWithPrefixValue(t *testing.T) {
-	a := new(Art)
-	k := []byte("B")
-	a.Insert([]byte("BBB"), "kk")
-	a.Insert(k, "k")
-	for i := 0; i < 256; i++ {
-		ck := append(k, byte(i))
-		a.Insert(ck, i)
-		hasByteKey(t, a, k, "k")
-		hasStringKey(t, a, "BBB", "kk")
-		for j := 0; j < i; j++ {
-			hasByteKey(t, a, append(k, byte(j)), j)
-		}
+	keyVals := []keyVal{
+		kvs("BBB", "kk"),
+		kvs("B", "k"),
+		kvs("BBx", 100),
 	}
+	for i := 0; i < 256; i++ {
+		keyVals = append(keyVals, kv([]byte{'B', byte(i)}, i))
+	}
+	testArt(t, keyVals, nil)
 }
 
 func Test_KeyWithZeros(t *testing.T) {
 	// any arbitrary byte array should be a valid key, even those with embedded nulls.
-	a := new(Art)
-	k1 := []byte{0, 0, 0}
-	k2 := []byte{0, 0, 0, 0}
-	k3 := []byte{0, 0, 0, 1}
-	a.Insert(k1, "k1")
-	a.Insert(k2, "k2")
-	a.Insert(k3, "k3")
-	hasByteKey(t, a, k1, "k1")
-	hasByteKey(t, a, k2, "k2")
-	hasByteKey(t, a, k3, "k3")
+	testArt(t, []keyVal{
+		kv([]byte{0, 0, 0}, "k1"),
+		kv([]byte{0, 0, 0, 0}, "k2"),
+		kv([]byte{0, 0, 0, 1}, "k3"),
+		kv([]byte{0, 1, 0}, "k4"),
+		kv([]byte{0, 1, 0, 1}, "k5"),
+	}, nil)
 }
 
 func Test_EmptyKey(t *testing.T) {
 	// an empty byte array is also a valid key
-	a := new(Art)
-	a.Insert(nil, "n")
-	hasByteKey(t, a, nil, "n")
-	a.Insert([]byte{}, "b")
-	hasByteKey(t, a, []byte{}, "b")
-	a.Insert([]byte{0}, "0")
-	hasByteKey(t, a, []byte{0}, "0")
-	hasByteKey(t, a, []byte{}, "b")
+	t.Run("nil key", func(t *testing.T) {
+		testArt(t, []keyVal{
+			kv(nil, "k1"),
+			kv([]byte{0}, "k2"),
+		}, nil)
+	})
+	t.Run("empty key", func(t *testing.T) {
+		testArt(t, []keyVal{
+			kv([]byte{}, "k1"),
+			kv([]byte{0}, "k2"),
+		}, nil)
+	})
+}
+
+func Test_NilValue(t *testing.T) {
+	testArt(t, []keyVal{
+		kv([]byte{0, 0, 0}, nil),
+		kv([]byte{0, 0, 0, 1}, "3"),
+		kv([]byte{10}, nil),
+	}, nil)
+}
+
+func Test_NodeCompression(t *testing.T) {
+	testArt(t, []keyVal{
+		kvs("1234567", "1"),
+		kvs("1239000", "1"),
+	}, &Stats{Node4s: 1, Leafs: 2, Keys: 2})
+}
+
+func Test_LeafExpansion(t *testing.T) {
+	testArt(t, []keyVal{
+		kvs("start", "foo"),
+		kvs("start more", "bar"),
+		kvs("start more and more", "baz"),
+	}, nil)
 }
 
 func Test_Walk(t *testing.T) {
-	a := new(Art)
-	a.Insert([]byte("C"), "c")
-	a.Insert([]byte("A"), "a")
-	a.Insert([]byte("AA"), "aa")
-	a.Insert([]byte("B"), "b")
-	expKeys := [][]byte{[]byte("A"), []byte("AA"), []byte("B"), []byte("C")}
-	expVals := []string{"a", "aa", "b", "c"}
-	idx := 0
-	a.Walk(func(k []byte, v interface{}) WalkState {
-		if !bytes.Equal(expKeys[idx], k) {
-			t.Errorf("At iteration %d expecting key %v but got %v", idx, expKeys[idx], k)
-		}
-		if v != expVals[idx] {
-			t.Errorf("At iteration %d expected value %v but got %v", idx, expVals[idx], v)
-		}
-		idx++
-		return Continue
-	})
-	if idx != 4 {
-		t.Errorf("Expected 4 callbacks during walk, but only got %d", idx)
-	}
+	testArt(t, []keyVal{
+		kvs("C", "c"),
+		kvs("A", "a"),
+		kvs("AA", "aa"),
+		kvs("B", "b"),
+	}, nil)
 }
 
 func Test_MoreWalk(t *testing.T) {
@@ -245,6 +220,81 @@ func Test_MoreWalk(t *testing.T) {
 	}
 }
 
+type keyVal struct {
+	key []byte
+	val interface{}
+}
+
+func kv(k []byte, v interface{}) keyVal {
+	return keyVal{key: k, val: v}
+}
+func kvs(k string, v interface{}) keyVal {
+	return keyVal{key: []byte(k), val: v}
+}
+
+func reverse(kv []keyVal) []keyVal {
+	c := make([]keyVal, len(kv))
+	j := len(kv) - 1
+	for i := 0; i < len(kv); i++ {
+		c[j] = kv[i]
+		j--
+	}
+	return c
+}
+
+func hasKeyVals(t *testing.T, a *Art, kv []keyVal) {
+	// verifies that the tree matches the supplied set of kv's by using the Walk fn
+	exp := append([]keyVal{}, kv...)
+	sort.Slice(exp, func(i, j int) bool {
+		return bytes.Compare(exp[i].key, exp[j].key) == -1
+	})
+	i := 0
+	a.Walk(func(k []byte, v interface{}) WalkState {
+		if !bytes.Equal(exp[i].key, k) {
+			t.Errorf("key %d was %v but expecting %v", i, k, exp[i].key)
+		}
+		if v != exp[i].val {
+			t.Errorf("key %v expecting value %v but was %v", exp[i].key, exp[i].val, v)
+		}
+		i++
+		return Continue
+	})
+	if i < len(kv) {
+		t.Errorf("Expecting %d keys to be walked, but only got %d", len(kv), i)
+	}
+}
+
+func testArt(t *testing.T, inserts []keyVal, expectedStats *Stats) {
+	t.Run("normal", func(t *testing.T) {
+		testArtOne(t, inserts, expectedStats)
+	})
+	t.Run("reverse insertion order", func(t *testing.T) {
+		testArtOne(t, reverse(inserts), expectedStats)
+	})
+}
+
+func testArtOne(t *testing.T, inserts []keyVal, expectedStats *Stats) {
+	a := new(Art)
+	for i := 0; i < len(inserts); i++ {
+		a.Insert(inserts[i].key, inserts[i].val)
+		for j := 0; j <= i; j++ {
+			hasByteKey(t, a, inserts[j].key, inserts[j].val)
+		}
+		hasKeyVals(t, a, inserts[:i+1])
+	}
+	if expectedStats != nil {
+		act := a.Stats()
+		if !reflect.DeepEqual(*expectedStats, *act) {
+			t.Errorf("Unexpected stats of %#v, expecting %#v", *act, *expectedStats)
+		}
+	}
+	if t.Failed() {
+		tree := &strings.Builder{}
+		a.PrettyPrint(tree)
+		t.Logf(tree.String())
+	}
+}
+
 func noStringKey(t *testing.T, a *Art, k string) {
 	t.Helper()
 	noByteKey(t, a, []byte(k))
@@ -268,20 +318,11 @@ func hasStringKey(t *testing.T, a *Art, k string, exp interface{}) {
 
 func hasByteKey(t *testing.T, a *Art, k []byte, exp interface{}) {
 	t.Helper()
-	act, exists := a.Get([]byte(k))
+	act, exists := a.Get(k)
 	if !exists {
 		t.Errorf("Should contain value for key %#v but does not", k)
 	}
 	if act != exp {
 		t.Errorf("Unexpected value of %#v for key %#v, expecting %#v", act, k, exp)
-	}
-
-func expectStats(t *testing.T, a *Art, exp Stats) {
-	act := a.Stats()
-	if !reflect.DeepEqual(&exp, act) {
-		t.Errorf("Unexpected stats of %#v, expecting %#v", *act, exp)
-		w := new(strings.Builder)
-		a.PrettyPrint(w)
-		t.Logf("tree is\n%v", w.String())
 	}
 }
